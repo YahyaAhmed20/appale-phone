78b1a92d27680d3eb714985ad014e1abd9606653
Yahya Ahmed <Yahya.Ahmed@DESKTOP>
2025-06-14 00:38:32 +0300

    Home Page

--- home/admin.py
+++ home/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
+from .models import CarouselItem
+from .models import ShopSingle
 
-# Register your models here.
+@admin.register(CarouselItem)
+class CarouselItemAdmin(admin.ModelAdmin):
+    list_display = ('title', 'is_active', 'order')
+    list_editable = ('is_active', 'order')
+    search_fields = ('title', 'subtitle')
+    prepopulated_fields = {'subtitle': ('title',)}  # اختياري
+    
+    
+    
+
+@admin.register(ShopSingle)
+class ShopSingleAdmin(admin.ModelAdmin):
+    list_display = ('title', 'price', 'brand', 'owner', 'rating')
+    search_fields = ('title', 'brand', 'owner__username')
+    list_filter = ('brand', 'owner')
+    prepopulated_fields = {'description': ('title',)}  # اختياري
--- home/views.py
+++ home/views.py
@@ -1,7 +1,18 @@
 from django.shortcuts import render
+from .models import CarouselItem
+from .models import ShopSingle
 
 # Create your views here.
 
 def home(request):
     
-    return render(request, 'home/home.html')
+    
+    carousel_items = CarouselItem.objects.filter(is_active=True)
+
+    products = ShopSingle.objects.all()
+
+    
+    return render(request, 'home/home.html', {
+        'carousel_items': carousel_items,
+        'products': products
+    })
