8a933023a816123625a6fcdfb74a86abb90b9933
Yahya Ahmed <Yahya.Ahmed@DESKTOP>
2025-06-22 00:40:28 +0300

    Deploy to Railway

--- project/settings.py
+++ project/settings.py
@@ -55,8 +55,7 @@ INSTALLED_APPS = [
 
     'allauth.socialaccount',
     'allauth.socialaccount.providers.google',
-    'cloudinary',
-    'cloudinary_storage',
+   
 
 
     
@@ -102,13 +101,6 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = 'project.wsgi.application'
-DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
-
-CLOUDINARY_STORAGE = {
-    'CLOUD_NAME': 'disv0yzel',
-    'API_KEY': '558685449993528',
-    'API_SECRET': 'ol-DZZrg9NdlzE9mzAtAgdL3Kw8',
-}
 
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
