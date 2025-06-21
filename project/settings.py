b9250caada819a489f8388cda4bf8198cd8f1af5
Yahya Ahmed <Yahya.Ahmed@DESKTOP>
2025-06-22 00:23:13 +0300

    Deploy to Railway

--- project/settings.py
+++ project/settings.py
@@ -148,6 +148,7 @@ USE_I18N = True
 
 USE_TZ = True
 
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
